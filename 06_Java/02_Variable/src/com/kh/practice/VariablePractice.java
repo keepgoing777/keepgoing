b7288a4919765042172cb10d28dc670c7e6ebc3e
keepgoing777 <babywithyou00@gmail.com>
2025-06-17 08:22:51 +0900

    자바
    6/16

---
+++ 06_Java/02_Variable/src/com/kh/practice/VariablePractice.java
@@ -0,0 +1,147 @@
+package com.kh.practice;
+
+import java.util.Scanner;
+
+public class VariablePractice {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		VariablePractice v = new VariablePractice();
+		v.method1();
+		v.method2();
+		v.method3();
+		v.method4();
+		v.method5();
+		v.method6();
+		v.method7();
+	}
+	
+	/*
+	 * 영화관의 요금표는 다음과 같다.
+	 *  - 성인 : 10000원
+	 *  - 청소년 : 7000원
+	 * 
+	 * 성인 2명과 청소년 3명이 영화를 보려고 할 때 지불해야 할 금액을 계산 후 출력하세요.
+	 * */
+	
+	
+	public void method1() {
+		int old = 10000;
+		int new2 = 7000;
+		int result = old*2 + new2*3;
+		System.out.println(result);
+	}
+	
+	
+	
+	/*
+	 * x=5, y=7, z=9의 값을 직접 변경하지 않고 변수를 사용하여
+	 * 
+	 * x=7
+	 * y=9
+	 * z=5
+	 * 
+	 * 로 출력하세요.
+	 * */
+	public void method2() {
+		int x = 5;
+		int y = 7;
+		int z = 9;
+		
+		
+
+	}
+	
+	
+
+	/*
+	 * 정수 두 개를 입력 받아 두 수의 합(+), 차(-), 곱(*), 나누기(/)한 몫을 출력하세요.
+	 * 
+	 * 첫 번째 정수 : 23
+	 * 두 번째 정수 : 7
+	 * 더하기 : 30
+	 * 빼기 : 16
+	 * 곱하기 : 161
+	 * 나누기 몫 : 3
+	 * */
+	public void method3() {
+		int num1 = 23;
+		int num2 = 7;
+		int result1 = num1 + num2;
+		int result2 = num1 - num2;
+	    int result3 = num1 * num2;	
+		int result4 = floor(num1%num2);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+	    System.out.println(result3);
+		System.out.println(result4);
+	}
+
+	/*
+	 * 가로, 세로 값을 입력 받아 사각형의 면적과 둘레를 계산하여 출력하세요.
+	 * 공식) 면적 : 가로 * 세로
+	 *       둘레 : (가로 + 세로) * 2
+	 * 
+	 * 가로 : 13.5
+	 * 세로 : 41.7
+	 * 면적 : 562.95
+	 * 둘레 : 110.4
+	 * */
+	public void method4() {
+		
+		 float row = 13.5;
+	     float column = 41.7;
+		 float s = 562.95;
+		 float r = 110.4;
+		
+		   s = row * column;
+		   r = (row + column) *2 ;
+
+	}
+
+	/*
+	 * 영어 문자열 값을 입력 받아 문자에서 첫번째, 두번째, 마지막 글자(문자열.length())를 출력하세요.
+	 * 
+	 * 문자열을 입력하세요 : apple
+	 * 첫번째 문자 : a
+	 * 두번째 문자 : p
+	 * 마지막 문자 : e
+	 * */
+	public void method5() {
+
+	}
+
+	/*
+	 * 문자 하나를 입력 받아 그 문자와 다음 문자의 유니코드를 출력하세요.
+	 * 
+	 * 문자 : A
+	 * A unicode : 65
+	 * B unicode : 66
+	 * */
+	public void method6() {
+
+	}
+	
+	/*
+	 * 국어, 영어, 수학 세 과목의 점수를 입력 받아 총점과 평균을 출력하세요.
+	 * 
+	 * 국어 : 75
+	 * 영어 : 63
+	 * 수학 : 80
+	 * 총점 : 218
+	 * 평균 : 72.67
+	 * */
+	public void method7() {
+		
+		int kor = 75;
+		int eng = 63;
+		int math = 80;
+		int total = 75 + 63 + 80 = 218
+		int ave = total / 3 
+		System.out.println(ave);
+
+	}
+
+}
---
+++ 06_Java/02_Variable/src/variable/A_variable.java
@@ -0,0 +1,114 @@
+package variable;
+/*
+ * 변수(Variable) : 값을 저장하는 "공간"
+ * */
+public class A_variable {
+
+	public static void main(String[] args) {
+
+		A_variable variable = new A_variable();
+		//실생할 메서드가 있는 클래스 생성 (new)
+		
+		//variable.printValue();
+		//생성한 클래스로 메서드 실행 (출력)
+	    //variable.variableTest();
+	   	 variable.constant();
+	}
+	
+	public void printValue() {
+		//원의 둘레와 원의 넓이를 출력
+		//원의 둘레 = 반지름 *2 * 3.141592
+		//원의 넓이 = 반지름 * 반지름 * 3.141592
+		
+		System.out.println(30 * 2 * 3.141592);
+		System.out.println(30 * 30 * 3.141592);
+		
+    //변수 사용해서 
+		int r = 30;
+		double pi = 3.141592;
+		
+		System.out.println(r*2*pi);
+		System.out.println(r*r*pi);
+    //syso 컨트롤+스페이스		
+	}
+
+    public void variableTest() {
+    	/*
+    	 * 변수의 선언
+    	 * 
+    	 * 자료형 변수명;
+    	 *  - 자료형 : 어떤 값을 저장할지, 어떤 크기에 메모리를 할당받을지에 대한 정보
+    	 *  - 변수명 : 변수의 이름으로 값을 읽고, 쓰고 지우는 작업을 할 때 사용
+    	 *  
+    	 * 변수의 초기화
+    	 *  - 변수명=값;
+    	 *  - 변수에 처음에 값을 저장하는 것 
+    	 *  - '='는 오른쪽 값을 왼쪽 변수에 대입(저장)하는 연산자
+    	 */ 
+    	 //기본 자료형(Primitive type) : 실제 값을 저장
+    	 //스택 메모리에 생성된 공간에 실제 변수값이 저장
+    	 // 1. 논리형 ( 1byte = 8bit 만큼의 공간 크기를 가짐)
+    	 boolean isTrue; //변수 선언
+    	 isTrue = true; //변수 초기화
+    	 boolean isFalse = false; //변수 선언과 함께 초기화
+    	 // 2. 문자형(
+    	 char ch = 'a';
+    	 // ch = 'ab'; 에러발생
+    	 // 3. 정수형
+    	 byte bNum = 1; //1byte(=8bit) -> 2^8 = 256(-123~127)
+    	 //bNum = 128; -> 에러발생, 오버플로우(overflow) 
+    	 short sNum = 128; // 2byte(16bit)
+    	 int iNum = 922337283; //4byte
+    	 long lNum = 922337283458L; //8byte - 숫자뒤에 L(l)을 입력해야함
+        	 System.out.println(lNum);
+       	 //4. 실수형
+    	 float fNum = 1.2F; // 4bite 숫자 뒤에 F(f)입력
+    	 double dNum = 1.2; // 8bite (기본)
+    	     System.out.println(fNum);
+    	 //5. 참조 자료형(클래스) : 4byte, 기본형을 제외한 나머지, 주소값 저장
+    	 //실제 데이터 값은 힙 메모리에 저장하고, 스택메모리의 변수공간에는 실제 변수값이 저장된 힙 메모리의 위치값 저장
+    	  String name = "이진용";
+    	  String name2 = new String("이진용");
+    	  System.out.println(name2);
+    	  
+    	 // 변수의 네이밍 규칙
+    	 // 1. 대소문자 구분
+    	  int number; int NUMBER;
+    	  int Number; //모두 다르게 인식 
+    	//String number; // 자료형이 달라도 변수명이 같으면 에러
+      	 // 2. 숫자로 시작하면 안된다.
+        //int 1age; //숫자로 시작해서
+    	  int a2g3e4; //숫자 섞이는 것은 상관없음
+    	 // 3. 특수문자는 '_', '$'만 사용 가능
+    	  int _age;
+    	  int age$; 
+        //int %age; // % 에러
+    	 // 4. 예약어(Reserved word)를 사용하면 안된다.
+        //int public;
+        //int void;
+    	  int void2;
+    	 // 5. (권장사항) 낙타표기법(카멜케이스)
+    	 // 첫글자는 항상 소문자, 여러 단어가 오면 대문자
+          int maxnumber; 
+          int max_number; //자바에서 X, 파이썬에서 O (스네이크 기법)
+    	  int maxNumber; //자바에서! 
+    }
+          public void constant() {
+         	 /*
+         	  * 상수
+         	  * final 자료형 변수명;
+         	    - 변하지 않는 값으로 특정 변수의 값이 변경되는 걸 원하지 않을 때 사용
+         	    - 초기화 이후에는 값을 변경할 수 없다.
+         	    - 상수의 네이밍 규칙은 대문자로 표기, 여러 단어가 오면 _로 구분
+        	  * */
+        	  int age = 20;
+        	  age = 25;
+        	  System.out.println(age);
+        	 
+        	  final int MAX_AGE = 25;
+        	  //MAX_AGE = 20;
+        	  System.out.println(MAX_AGE);
+          }
+}
+
+
